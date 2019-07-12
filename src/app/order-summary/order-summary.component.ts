e85dcccc763186ed02ebb9659e689877feb1c124
Ernani Cubas Neto <escneto@mppr.mp.br>
2019-07-12 14:07:46 -0300

    Alterações Neto

---
+++ src/app/order-summary/order-summary.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { TimerObservable } from 'rxjs/observable/TimerObservable';
+
+@Component({
+  selector: 'mt-order-summary',
+  templateUrl: './order-summary.component.html'
+})
+export class OrderSummaryComponent implements OnInit {
+
+  rated:boolean
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  rate(){
+    this.rated = true
+  }
+
+}
